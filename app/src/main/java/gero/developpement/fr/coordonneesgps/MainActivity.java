b779408375bd48c622ee977e9d20b12a3aa1cd2b
yvan <yvan.maillot@gmail.com>
2016-04-15 18:47:07 +0200

    Utilisation de la ressource LocationManager pour afficher quelques informations dans le log

--- app/src/main/java/gero/developpement/fr/coordonneesgps/MainActivity.java
+++ app/src/main/java/gero/developpement/fr/coordonneesgps/MainActivity.java
@@ -1,7 +1,13 @@
 package gero.developpement.fr.coordonneesgps;
 
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +20,35 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        
+        Log.v("Coordonnées GPS", "-----------------");
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        List<String> liste = locationManager.getAllProviders();
+        for(String s : liste) {
+
+            Log.v("Coordonnées GPS", "-----------------");
+            Log.v("Coordonnées GPS", s);
+            Log.v("Coordonnées GPS", "-----------------");
+
+            try {
+                Location location = locationManager.getLastKnownLocation(s);
+                Log.v("Coordonnées GPS", location.toString());
+                Log.v("Coordonnées GPS", "Précision : " + location.getAccuracy());
+                Log.v("Coordonnées GPS", "Altitude : " + location.getAltitude());
+                Log.v("Coordonnées GPS", "Vitesse : " + (location.hasSpeed()?"OUI -> ":"NON <- ") + location.getSpeed());
+                Log.v("Coordonnées GPS", "Latitude : " + location.getLatitude());
+                Log.v("Coordonnées GPS", "Longitude : " + location.getLongitude());
+            } catch (SecurityException se) {
+
+            }
+            Log.v("Coordonnées GPS", "-----------------");
+
+        }
+        Criteria c = new Criteria();
+        c.setAccuracy(Criteria.ACCURACY_FINE);
+        c.setAltitudeRequired(true);
+        c.setCostAllowed(false);
+
+        Log.v("Coordonnées GPS", "The best : " + locationManager.getBestProvider(c, true));
+
     }
 }
